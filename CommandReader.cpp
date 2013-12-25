f91b7bde7abbfa1496d7038a3fd56493ab2f13a3
JeanMarchal <jean.marchal@hotmail.com>
2013-12-26 00:20:42 +0100

    Modification CommandReader
    Compilation, correction des erreurs (non finis)

--- CommandReader.cpp
+++ CommandReader.cpp
@@ -4,66 +4,116 @@
 using namespace std;
 
 
-commandReader () {};
+CommandReader::CommandReader ( )
+{
 
-virtual ~commandReader () {};
+}
 
-void readCommand (const char cmd [])
+CommandReader::~CommandReader ( )
+{
+
+}
+
+void CommandReader::readCommand (const char cmd [])
 {
 	typeCommand cmdName;
 	bool testArgument(true);
 	string cmdstr(cmd);
 	getline (cmd, cmdName, ' ');
-	switch (cmdName)
-		case 'C':
-			testArgument = argumentNumber (cmd);
-			cmdstr.erase(0, cmdstr.find(' '));
-			int x;
-			int y;
-			if (testArgument)
-			{
-				defineCoordonnee (cmdstr,cmdName);
-				//Problème pour la récupération des coordonnées.
-			}
-			break;
-		case 'R':
-			testArgument = argumentNumber (cmd);
-			break;
-		case 'L':
-			testArgument = argumentNumber (cmd);
-			break;
-		case "PL":
-			testArgument = argumentNumber (cmd);
-			break;
-		case "OA":
-			testArgument = argumentNumber (cmd);
-			break;
-		case "DELETE":
-			testArgument = argumentNumber (cmd);
-			break;
-		case "MOVE":
-			testArgument = argumentNumber (cmd);
-			break;
-		case "LIST":
-			testArgument = argumentNumber (cmd);
-			break;
-		case "UNDO":
-			testArgument = argumentNumber (cmd);
-			break;
-		case "REDO":
-			testArgument = argumentNumber (cmd);
-			break;
-		case "LOAD":
-			testArgument = argumentNumber (cmd);
-			break;
-		case "SAVE":
-			testArgument = argumentNumber (cmd);
-			break;
-		case "CLEAR":
-			testArgument = argumentNumber (cmd);
-			break;
-		case "EXIST":		
-			testArgument = argumentNumber (cmd);
-			break;
-	
+	if (cmdName=='C')
+    {
+        cmdstr.erase(0, cmdstr.find(' '));
+        testArgument = argumentNumber (cmdstr);
+        int x;
+        int y;
+        if (testArgument)
+        {
+            defineCoordonnee (cmdstr,cmdName);
+            //Problème pour la récupération des coordonnées
+            //Sans doute : déclaration des variables au préalable et remplacement des valeurs via cette fonction
+            //Problèmes lorsque le nombre de coordonnées est inconnu
+        }
+    }
+    else if (cmdName=='R')
+    {
+        cmdstr.erase(0, cmdstr.find(' '));
+        testArgument = argumentNumber (cmdstr);
+    }
+    else if (cmdName=='L')
+	{
+    	cmdstr.erase(0, cmdstr.find(' '));
+        testArgument = argumentNumber (cmdstr);
+	}
+	else if (cmdName=="PL")
+	{
+        cmdstr.erase(0, cmdstr.find(' '));
+        testArgument = argumentNumber (cmdstr);
+	}
+	else if (cmdName=="OA")
+	{
+    	cmdstr.erase(0, cmdstr.find(' '));
+        testArgument = argumentNumber (cmdstr);
+	}
+	else if (cmdName=="DELETE")
+	{
+    	cmdstr.erase(0, cmdstr.find(' '));
+        testArgument = argumentNumber (cmdstr);
+	}
+	else if (cmdName=="MOVE")
+	{
+	    cmdstr.erase(0, cmdstr.find(' '));
+        testArgument = argumentNumber (cmdstr);
+	}
+	else if (cmdName=="LIST")
+	{
+    	cmdstr.erase(0, cmdstr.find(' '));
+		testArgument = argumentNumber (cmdstr);
+	}
+	else if (cmdName=="UNDO")
+	{
+    	cmdstr.erase(0, cmdstr.find(' '));
+        testArgument = argumentNumber (cmdstr);
+	}
+	else if (cmdName=="REDO")
+	{
+    	cmdstr.erase(0, cmdstr.find(' '));
+		testArgument = argumentNumber (cmdstr);
+	}
+	else if (cmdName=="LOAD")
+	{
+    	cmdstr.erase(0, cmdstr.find(' '));
+		testArgument = argumentNumber (cmdstr);
+	}
+	else if (cmdName=="SAVE")
+	{
+    	cmdstr.erase(0, cmdstr.find(' '));
+		testArgument = argumentNumber (cmdstr);
+	}
+	else if (cmdName=="CLEAR")
+	{
+	    cmdstr.erase(0, cmdstr.find(' '));
+		testArgument = argumentNumber (cmdstr);
+	}
+	else if (cmdName=="EXIST")
+	{
+	    cmdstr.erase(0, cmdstr.find(' '));
+		testArgument = argumentNumber (cmdstr);
+	}
+	else
+    {
+        cerr<<"Instuction envoyée incorrecte";
+        //Pourra être modifiée en une autre instruction en fonction du type de retour de la fonction.
+	}
+
 }
+
+void CommandReader::defineCoordonnee (string cmd, typeCommand & cmdName)
+{
+
+}
+
+bool CommandReader::argumentNumber (string cmdLine)
+{
+
+}
+
--- CommandReader.h
+++ CommandReader.h
@@ -1,47 +1,47 @@
-#ifndef COMMANDREADER_H
+#if ! defined (COMMANDREADER_H)
 #define COMMANDREADER_H
 
 #include <string>
+using namespace std;
 
-typedef enum typeCommand typeCommand;
 enum typeCommand :string
 {
-	C;
-	R;
-	L;	
-	PL;
-	OA;
-	DELETE;
-	MOVE;
-	LIST;
-	UNDO;
-	REDO;
-	LOAD;
-	SAVE;
-	CLEAR;
-	EXIT; 
+	C,
+	R,
+	L,
+	PL,
+	OA,
+	DELETE,
+	MOVE,
+	LIST,
+	UNDO,
+	REDO,
+	LOAD,
+	SAVE,
+	CLEAR,
+	EXIT,
 };
 
-class CommandReader :
+class CommandReader
 {
 	public:
-		commandReader ();
-		virtual ~commandReader ();
-		
+		CommandReader ( );
+		virtual ~CommandReader ( );
+
 		// fonction qui lit la ligne de commande rentrée en paramètre (const char []).
 		void readCommand (const char cmd []);
-		
+
 		// Fonction qui prend en paramètre la ligne de commande entrée et l'instruction géométrique.
 		// Si la ligne de commande est correcte, renvoie les coordonnées de la forme géométrique.
 		void defineCoordonnee (string cmd, typeCommand & cmdName);
-		
+
 		// Compte les arguments de la ligne de commande en paramètre et teste leur type.
 		// Si la ligne de commande renvoyée est correcte, renvoie true.
-		bool argumentNumber (const char cmdLine []);
-		
+		bool argumentNumber (string cmdLine);
+
 	protected:
 
 
-}
+};
 
 #endif
